8fd28bf6f1c37151c506600d7570d91c6ae884c9
Zageron <hello@zageron.com>
2022-08-02 10:24:30 -0700

    Add results screen prototype.

---
+++ src/components/results/results.ts
@@ -0,0 +1,111 @@
+import type { HTMLTemplateResult } from 'lit'
+import { html } from 'lit'
+import { customElement } from 'lit/decorators.js'
+
+import { TwLitElement } from '../../../dist/components/tw-lit-element'
+
+/*
+ * https://play.tailwindcss.com/gPKxLd59sP
+ */
+@customElement('x-results-element')
+export class ResultsElement extends TwLitElement {
+  protected render(): HTMLTemplateResult {
+    return html`
+      <div
+        class="relative mx-auto flex max-h-rrr min-h-rrr min-w-rrr max-w-rrr flex-col bg-black"
+      >
+        <div class="relative flex h-14 flex-row justify-between bg-slate-400">
+          <div class="flex flex-row items-center">
+            <div>results for zageron</div>
+            <div>play random song</div>
+            <div>save replay</div>
+            <div>take screenshot</div>
+          </div>
+          <div class="flex items-center"><div>current timestamp</div></div>
+        </div>
+        <div class="relative flex flex-1 flex-col bg-slate-200">
+          <div class="flex flex-col items-center">
+            <div>Name of song</div>
+            <div>difficulty, length, artist, chartist</div>
+          </div>
+          <div class="relative flex flex-1 flex-row bg-slate-500">
+            <div
+              class="relative flex flex-col items-end justify-center bg-yellow-400 p-4"
+            >
+              <div class="flex flex-row items-end">
+                <div>Amazing</div>
+                <div class="text-xl">0</div>
+              </div>
+              <div class="flex flex-row items-end">
+                <div>Perfect</div>
+                <div class="text-xl">0</div>
+              </div>
+              <div class="flex flex-row items-end">
+                <div>Good</div>
+                <div class="text-xl">0</div>
+              </div>
+              <div class="flex flex-row items-end">
+                <div>Average</div>
+                <div class="text-xl">0</div>
+              </div>
+              <div class="flex flex-row items-end">
+                <div>Miss</div>
+                <div class="text-xl">0</div>
+              </div>
+              <div class="flex flex-row items-end">
+                <div>Boo</div>
+                <div class="text-xl">0</div>
+              </div>
+            </div>
+            <div class="relative flex flex-1">
+              <div
+                class="relative flex flex-1 flex-col items-center justify-center bg-yellow-600"
+              >
+                <div class="flex flex-row items-end">
+                  <div>Amazing</div>
+                  <div class="text-xl">0</div>
+                </div>
+                <div class="flex flex-row items-end">
+                  <div>AAA Equivalency:</div>
+                  <div class="text-xl">0</div>
+                </div>
+                <div class="flex flex-row items-end">
+                  <div>Raw Goods:</div>
+                  <div class="text-xl">0</div>
+                </div>
+                <div class="flex flex-row items-end">
+                  <div>Score:</div>
+                  <div class="text-xl">0</div>
+                </div>
+                <div class="flex flex-row items-end">
+                  <div>Grandtotal:</div>
+                  <div class="text-xl">0</div>
+                </div>
+                <div class="flex flex-row items-end">
+                  <div>Max Combo:</div>
+                  <div class="text-xl">0</div>
+                </div>
+                <div class="flex flex-row items-end">
+                  <div>Credits:</div>
+                  <div class="text-xl">0</div>
+                </div>
+              </div>
+            </div>
+            <div class="flex items-center p-4">
+              <div class="box-border h-32 w-32 border-2"></div>
+            </div>
+          </div>
+          <div>Mods and Game Settings</div>
+        </div>
+        <div class="relative flex flex-1 bg-slate-600">Judgement Graph</div>
+        <div class="relative flex flex-1 bg-slate-700">Menu</div>
+      </div>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'x-results-element': ResultsElement
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { BackgroundElement } from './components/background/background'
 export { FullElement } from './components/full/full'
 export { PlayElement } from './components/play/play'
+export { ResultsElement } from './components/results/results'
