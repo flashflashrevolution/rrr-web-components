a0b449b28d4a2be3161a7099070023ad9902c23b
Zageron <hello@zageron.com>
2022-10-01 19:07:04 -0700

    fix(lit): destroy the render root of play to enable testing

--- src/components/play/play.ts
+++ src/components/play/play.ts
@@ -15,6 +15,10 @@ export default class Play extends TwLitElement {
 
   canvas: HTMLCanvasElement | undefined
 
+  createRenderRoot() {
+    return this
+  }
+
   async firstUpdated() {
     var engine = new rrr.Engine()
     this.canvas = this.renderRoot!.querySelector('#rrr') as
